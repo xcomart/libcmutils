0fd96eedf91a6ebeb894d6da9ccb936cda65a3a5
Dennis Soungjin Park <xcomart@gmail.com>
2018-09-13 07:18:25 +0900

    building...

--- src/http.c
+++ src/http.c
@@ -14,6 +14,32 @@ static const char *g_cmutil_http_methods[] = {
 
 typedef struct CMUTIL_HttpMethod_Internal {
     CMUTIL_HttpMethod   base;
-    const char          *methodstr;
-    
+    CMHttpMethodType    methodType;
+    CMUTIL_ByteBuffer   *rawData;
+    CMUTIL_JsonObject   *params;
+    CMUTIL_JsonObject   *headers;
 } CMUTIL_HttpMethod_Internal;
+
+CMUTIL_HttpMethod *CMUTIL_HttpMethodAddFormData(
+        CMUTIL_HttpMethod *method, const char *name, CMUTIL_String *data)
+{
+    CMUTIL_HttpMethod_Internal *mi = (CMUTIL_HttpMethod_Internal*)method;
+    CMCall(mi->params, PutString, name, CMCall(data, GetCString));
+    return method;
+}
+
+CMUTIL_HttpMethod *CMUTIL_HttpMethodSetRawData(
+        CMUTIL_HttpMethod *method, CMUTIL_ByteBuffer *data)
+{
+    CMUTIL_HttpMethod_Internal *mi = (CMUTIL_HttpMethod_Internal*)method;
+    mi->rawData = data;
+    return method;
+}
+
+CMUTIL_HttpMethod *CMUTIL_HttpMethodSetRequestHeader(
+        CMUTIL_HttpMethod *method, const char *name, CMUTIL_String *data)
+{
+    CMUTIL_HttpMethod_Internal *mi = (CMUTIL_HttpMethod_Internal*)method;
+    CMCall(mi->headers, PutString, name, CMCall(data, GetCString));
+    return method;
+}
