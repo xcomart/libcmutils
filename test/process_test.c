9c9ceff1c81e8fc5bef76809b7ce32eecdb92794
Dennis Park <xcomart@gmail.com>
2025-12-28 17:24:53 +0900

    process test more

--- test/process_test.c
+++ test/process_test.c
@@ -2,6 +2,8 @@
 // Created by 박성진 on 25. 12. 26..
 //
 
+#include <stdlib.h>
+
 #include "libcmutils.h"
 #include "test.h"
 
@@ -12,16 +14,28 @@ int main() {
     CMUTIL_Init(CMUTIL_MEM_TYPE);
 
     CMUTIL_Process *proc = NULL;
+    CMUTIL_Map *env = NULL;
 
-    proc = CMUTIL_ProcessCreate(
-        "/Users/comart", NULL, "/opt/homebrew/bin/openssl");
+    env = CMUTIL_MapCreateEx(CMUTIL_MAP_DEFAULT, CMFalse, CMFree);
+
+    const char *path = getenv("PATH");
+    CMUTIL_String *pathstr = CMUTIL_StringCreateEx(0, path);
+    CMCall(pathstr, AddString, ":/opt/homebrew/bin");
+    path = CMCall(pathstr, GetCString);
+    CMCall(env, Put, "PATH", CMStrdup(path), NULL);
+
+    const char *home = getenv("HOME");
+
+    proc = CMUTIL_ProcessCreate(home, env, "openssl", "help", "sha256");
     ASSERT(proc != NULL, "Process Create");
     ASSERT(CMCall(proc, Start, CMProcStreamNone), "Process Start");
 
-    ASSERT(CMCall(proc, Wait, 10*1000), "Process Wait");
+    ASSERT(CMCall(proc, Wait, 10*1000) == 0, "Process Wait");
 
     ir = 0;
 END_POINT:
+    if (pathstr) CMCall(pathstr, Destroy);
+    if (env) CMCall(env, Destroy);
     if (proc) CMCall(proc, Destroy);
     if (!CMUTIL_Clear()) ir = -1;
     return ir;
