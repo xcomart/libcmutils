cf2773c5ffbe9ec1d16cbf45081047070b5fc6d9
Dennis Park <xcomart@gmail.com>
2025-12-28 20:15:18 +0900

    windows bug fixed

--- src/process.c
+++ src/process.c
@@ -24,7 +24,6 @@ SOFTWARE.
 
 #include "functions.h"
 #include <stdint.h>
-#include <sys/poll.h>
 
 CMUTIL_LogDefine("cmutils.process")
 
@@ -37,6 +36,7 @@ typedef HANDLE CMStream;
 #include <sys/wait.h>
 #include <errno.h>
 #include <unistd.h>
+#include <sys/poll.h>
 
 typedef int CMStream;
 #define EMPTY_STREAM (-1)
@@ -125,11 +125,11 @@ CMUTIL_STATIC CMBool CMUTIL_StartSubprocess(CMUTIL_Process *proc)
     int i;
 
     // Create a pipe for the child process's STDIN.
-    CMUTIL_CreatePipe(p1, true, false);
+    CMUTIL_CreatePipe(p1, CMTrue, CMFalse);
     // Create a pipe for the child process's STDOUT.
-    CMUTIL_CreatePipe(p2, false, true);
+    CMUTIL_CreatePipe(p2, CMFalse, CMTrue);
     // Create a pipe for the child process's STDERR.
-    CMUTIL_CreatePipe(p3, false, true);
+    CMUTIL_CreatePipe(p3, CMFalse, CMTrue);
 
     ZeroMemory( &piProcInfo, sizeof(PROCESS_INFORMATION) );
     ZeroMemory( &siStartInfo, sizeof(STARTUPINFO) );
@@ -143,19 +143,19 @@ CMUTIL_STATIC CMBool CMUTIL_StartSubprocess(CMUTIL_Process *proc)
 
     cmd = CMCall(ip->command, Clone);
     for (i=0; i<CMCall(ip->args, GetSize); i++) {
-        CMUTIL_String *arg = CMCall(ip->args, GetAt, i);
+        const CMUTIL_String *arg = CMCall(ip->args, GetAt, i);
         CMCall(cmd, AddChar, ' ');
         CMCall(cmd, AddAnother, arg);
     }
 
-    BOOL bSuccess = CreateProcess(nullptr,
-                                  CMCall(cmd, GetCString), // command line
-                                  nullptr, // process security attributes
-                                  nullptr, // primary thread security attributes
+    BOOL bSuccess = CreateProcess(NULL,
+                                  (LPTSTR)CMCall(cmd, GetCString), // command line
+                                  NULL, // process security attributes
+                                  NULL, // primary thread security attributes
                                   TRUE, // handles are inherited
                                   CREATE_NO_WINDOW, // creation flags
-                                  nullptr, // use parent's environment
-                                  nullptr, // use parent's current directory
+                                  NULL, // use parent's environment
+                                  NULL, // use parent's current directory
                                   &siStartInfo, // STARTUPINFO pointer
                                   &piProcInfo);     // receives PROCESS_INFORMATION
     CMCall(cmd, Destroy);
@@ -163,25 +163,25 @@ CMUTIL_STATIC CMBool CMUTIL_StartSubprocess(CMUTIL_Process *proc)
         CMLogErrorS("CreateProcess failed");
         goto ERROR_POINT;
     }
-    hproc = piProcInfo.hProcess;
+    ip->hproc = piProcInfo.hProcess;
     CloseHandle(piProcInfo.hThread);
-    pid = piProcInfo.dwProcessId;
+    ip->pid = piProcInfo.dwProcessId;
     // these pipes are used in child process no longer needed in parent
 
     ip->inpipe = p1[1];
     ip->outpipe = p2[0];
     ip->errpipe = p3[0];
-    closePipe(p1[0]);
-    closePipe(p2[1]);
-    closePipe(p3[1]);
+    CMUTIL_ClosePipe(p1[0]);
+    CMUTIL_ClosePipe(p2[1]);
+    CMUTIL_ClosePipe(p3[1]);
     return CMTrue;
 ERROR_POINT:
-    closePipe(p1[0]);
-    closePipe(p1[1]);
-    closePipe(p2[0]);
-    closePipe(p2[1]);
-    closePipe(p3[0]);
-    closePipe(p3[1]);
+    CMUTIL_ClosePipe(p1[0]);
+    CMUTIL_ClosePipe(p1[1]);
+    CMUTIL_ClosePipe(p2[0]);
+    CMUTIL_ClosePipe(p2[1]);
+    CMUTIL_ClosePipe(p3[0]);
+    CMUTIL_ClosePipe(p3[1]);
 
     ip->inpipe = EMPTY_STREAM;
     ip->outpipe = EMPTY_STREAM;
@@ -307,9 +307,10 @@ CMUTIL_STATIC ssize_t CMUTIL_ProcessReadPipe(
 {
     ssize_t read_size = 0;
 #ifdef _WIN32
-    while (read_size < size) {
+    while (read_size < (ssize_t)size) {
         DWORD rsz = 0;
-        if (!ReadFile(strm, data+read_size, size-read_size, &rsz, nullptr)) {
+        if (!ReadFile(strm, data+read_size,
+            (ssize_t)size-read_size, &rsz, NULL)) {
             if (GetLastError() != ERROR_IO_PENDING) {
                 return -1;
             }
@@ -329,9 +330,10 @@ CMUTIL_STATIC ssize_t CMUTIL_ProcessWritePipe(
 {
     ssize_t write_size = 0;
 #ifdef _WIN32
-    while (write_size < size) {
+    while (write_size < (ssize_t)size) {
         DWORD wsz = 0;
-        if (!WriteFile(strm, data+write_size, size-write_size, &wsz, nullptr)) {
+        if (!WriteFile(strm, data+write_size,
+            (ssize_t)size-write_size, &wsz, NULL)) {
             DWORD err = GetLastError();
             if (err != ERROR_IO_PENDING) {
                 return -1;
@@ -376,10 +378,53 @@ CMUTIL_STATIC void *CMUTIL_ProcessReadProc(void *data)
         ip->memst, 1025);
     CMUTIL_ByteBuffer *errbuf = CMUTIL_ByteBufferCreateInternal(
         ip->memst, 1025);
+    int closecnt = 0;
+    int fd_cnt = 2;
+#if defined(MSWIN)
+    HANDLE pfd[2];
+    HANDLE *tpfd = pfd;
+    if (ip->type & CMProcStreamRead) {
+        fd_cnt--;
+        tpfd = pfd + 1;
+    }
+    while (closecnt < 3) {
+        uint8_t c;
+        DWORD rc = WaitForMultipleObjects(fd_cnt, tpfd, FALSE, 0);
+        if (rc >= WAIT_OBJECT_0 && rc <= WAIT_OBJECT_0 + fd_cnt - 1) {
+            if (rc == WAIT_OBJECT_0 && fd_cnt == 2) {
+                ssize_t sz = CMUTIL_ProcessReadPipe(ip->outpipe, &c, 1);
+                if (sz == 1) {
+                    CMCall(stdbuf, AddByte, c);
+                    if (CMCall(stdbuf, GetSize) == 1024) {
+                        CMUTIL_ProcessFlushStdout(ip, stdbuf);
+                    }
+                } else if (sz < 0) {
+                    CMLogError("read stdout error: %s", strerror(errno));
+                    closecnt |= 1;
+                }
+            } else if ((rc == WAIT_OBJECT_0 + 1 && fd_cnt == 2) ||
+                (rc == WAIT_OBJECT_0 && fd_cnt == 1)) {
+                ssize_t sz = CMUTIL_ProcessReadPipe(ip->errpipe, &c, 1);
+                if (sz == 1) {
+                    CMCall(errbuf, AddByte, c);
+                    if (CMCall(errbuf, GetSize) == 1024) {
+                        CMUTIL_ProcessFlushStderr(ip, errbuf);
+                    }
+                } else if (sz < 0) {
+                    CMLogError("read stderr error: %s", strerror(errno));
+                    closecnt |= 2;
+                }
+            }
+        } else if (rc == WAIT_FAILED) {
+            CMLogError("read stdout error: %s", strerror(errno));
+            closecnt = 3;
+        } else if (rc == WAIT_TIMEOUT) {
+            USLEEP(10000);
+        }
+    }
+#else
     struct pollfd pfd[2];
     struct pollfd *tpfd = pfd;
-    int fd_cnt = 2;
-    int closecnt = 0;
     memset(pfd, 0x0, sizeof(struct pollfd)*2);
 
     if (ip->type & CMProcStreamRead) {
@@ -441,10 +486,11 @@ CMUTIL_STATIC void *CMUTIL_ProcessReadProc(void *data)
             }
             USLEEP(10000);
         } else {
-            CMLogError("poll error: %s", strerror(errno));
+            CMLogError("rc(%d) poll error: %s", rc, strerror(errno));
             break;
         }
     }
+#endif
     // ip->status = CM_EXITED_;
     if (CMCall(stdbuf, GetSize) > 0) {
         CMUTIL_ProcessFlushStdout(ip, stdbuf);
@@ -452,6 +498,7 @@ CMUTIL_STATIC void *CMUTIL_ProcessReadProc(void *data)
     if (CMCall(errbuf, GetSize) > 0) {
         CMUTIL_ProcessFlushStderr(ip, errbuf);
     }
+
     CMCall(stdbuf, Destroy);
     CMCall(errbuf, Destroy);
     return NULL;
@@ -576,7 +623,7 @@ CMUTIL_STATIC ssize_t CMUTIL_ProcessWrite(
     CMUTIL_Process *proc, const void *buf, size_t count)
 {
     const CMUTIL_Process_Internal *ip = (CMUTIL_Process_Internal *)proc;
-    return write(ip->inpipe, buf, count);
+    return CMUTIL_ProcessWritePipe(ip->inpipe, buf, count);
 }
 
 CMUTIL_STATIC ssize_t CMUTIL_ProcessRead(
@@ -589,7 +636,7 @@ CMUTIL_STATIC ssize_t CMUTIL_ProcessRead(
     while (nread < count) {
         ssize_t remain = (ssize_t)count - nread;
         if (remain > sizeof(bytes)) remain = sizeof(bytes);
-        ir = read(ip->outpipe, bytes, remain);
+        ir = CMUTIL_ProcessReadPipe(ip->outpipe, bytes, remain);
         if (ir <= 0) break;
         CMCall(buf, AddBytes, bytes, ir);
         nread += ir;
@@ -605,7 +652,12 @@ CMUTIL_STATIC int CMUTIL_ProcessWait(CMUTIL_Process *proc, long millis)
     if (ip->status == CM_RUNNING_) {
 #if defined(MSWIN)
         if (ip->hproc) {
+            DWORD exit_code;
             WaitForSingleObject(ip->hproc, millis < 0 ? INFINITE : millis);
+            GetExitCodeProcess(ip->hproc, &exit_code);
+            status = (int)exit_code;
+        } else {
+            status = -1;
         }
 #else
         long counts = millis / 100;
@@ -721,37 +773,43 @@ CMUTIL_Process *CMUTIL_ProcessCreateInternal(
         const char *command,
         CMUTIL_StringArray *args)
 {
-    CMUTIL_Process_Internal *ip = (CMUTIL_Process_Internal *)memst->Alloc(
-        sizeof(CMUTIL_Process_Internal));
-    if (ip == NULL) {
-        CMLogErrorS("Failed to allocate memory.");
+    if (command == NULL || memst == NULL) {
+        CMLogErrorS("Invalid arguments. command or memst is NULL");
         return NULL;
-    }
-    memset(ip, 0x0, sizeof(CMUTIL_Process_Internal));
-    memcpy(ip, &g_cmutil_process, sizeof(CMUTIL_Process));
-    ip->memst = memst;
-    ip->cwd = CMUTIL_StringCreateInternal(memst, 0, cwd);
-    ip->env = env;
-    ip->command = CMUTIL_StringCreateInternal(memst, 0, command);
-    ip->args = args;
-    ip->status = CM_IDLE_;
-    ip->inpipe = EMPTY_STREAM;
-    ip->outpipe = EMPTY_STREAM;
-    ip->errpipe = EMPTY_STREAM;
-    ip->type = CMProcStreamNone;
-    if (CMLogIsEnabled(CMLogLevel_Debug)) {
-        int i;
-        CMUTIL_String *buf = CMUTIL_StringCreateInternal(memst, 1024, NULL);
-        CMCall(buf, AddAnother, ip->command);
-        for (i = 0; i < CMCall(args, GetSize); i++) {
-            const CMUTIL_String *arg = CMCall(args, GetAt, i);
-            CMCall(buf, AddChar, ' ');
-            CMCall(buf, AddAnother, arg);
+    } else {
+        CMUTIL_Process_Internal *ip = (CMUTIL_Process_Internal *)memst->Alloc(
+            sizeof(CMUTIL_Process_Internal));
+        if (ip == NULL) {
+            CMLogErrorS("Failed to allocate memory.");
+            return NULL;
         }
-        CMLogDebug("Creating process: %s", CMCall(buf, GetCString));
-        CMCall(buf, Destroy);
+        memset(ip, 0x0, sizeof(CMUTIL_Process_Internal));
+        memcpy(ip, &g_cmutil_process, sizeof(CMUTIL_Process));
+        ip->memst = memst;
+        if (cwd)
+            ip->cwd = CMUTIL_StringCreateInternal(memst, 0, cwd);
+        ip->env = env;
+        ip->command = CMUTIL_StringCreateInternal(memst, 0, command);
+        ip->args = args;
+        ip->status = CM_IDLE_;
+        ip->inpipe = EMPTY_STREAM;
+        ip->outpipe = EMPTY_STREAM;
+        ip->errpipe = EMPTY_STREAM;
+        ip->type = CMProcStreamNone;
+        if (CMLogIsEnabled(CMLogLevel_Debug)) {
+            int i;
+            CMUTIL_String *buf = CMUTIL_StringCreateInternal(memst, 1024, NULL);
+            CMCall(buf, AddAnother, ip->command);
+            for (i = 0; i < CMCall(args, GetSize); i++) {
+                const CMUTIL_String *arg = CMCall(args, GetAt, i);
+                CMCall(buf, AddChar, ' ');
+                CMCall(buf, AddAnother, arg);
+            }
+            CMLogDebug("Creating process: %s", CMCall(buf, GetCString));
+            CMCall(buf, Destroy);
+        }
+        return (CMUTIL_Process *)ip;
     }
-    return (CMUTIL_Process *)ip;
 }
 
 CMUTIL_Process *CMUTIL_ProcessCreateEx(
